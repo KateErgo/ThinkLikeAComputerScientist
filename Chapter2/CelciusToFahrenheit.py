402a5ca10192569a8a0ce7fabdb0eb716555044b
Kate Ergo <32951074+KateErgo@users.noreply.github.com>
2018-10-28 17:47:21 +0100

    Add files via upload

---
+++ Chapter2/CelciusToFahrenheit.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 17:42:04 2018
+
+@author: kate
+"""
+
+##########################################
+#### Think Like A Computer Sciencitst ####
+##########################################
+
+### Chapter 2 ###
+
+## Celcius To Fahrenheit ##
+
+def toFahrenheit():
+    
+    C = int(input("\nEnter degrees in Celcius: "))
+    
+    F = (C * 1.8) + 32
+    
+    return F
+
+fahrenheit = toFahrenheit()
+print("%s degrees in Celcius equals %.0f degrees in Fahrenheit." %(C,F))
