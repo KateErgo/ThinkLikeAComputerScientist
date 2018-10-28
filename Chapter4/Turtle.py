69d19c64c33ed4e954038c038bb6e742eb8b8914
Kate Ergo <32951074+KateErgo@users.noreply.github.com>
2018-10-28 18:01:35 +0100

    Add files via upload

---
+++ Chapter4/Turtle.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 17:57:34 2018
+
+@author: kate
+"""
+
+##########################################
+#### Think Like A Computer Sciencitst ####
+##########################################
+
+### Chapter 4 ###
+
+## Turtles ##
+
+def turtles():
+    
+    n = int(input("\nHow many times should 'turtle' be printed? "))
+    
+    allTurtles = 'turtle\n' * n
+    
+    print(allTurtles)
+    
+turtles()
