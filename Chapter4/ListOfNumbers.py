ae15d263d5e654480e39cae2c57f14c54c4bc4f2
Kate Ergo <32951074+KateErgo@users.noreply.github.com>
2018-10-29 19:54:08 +0100

    Add files via upload

---
+++ Chapter4/ListOfNumbers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 19:51:22 2018
+
+@author: kate
+"""
+
+##########################################
+#### Think Like A Computer Sciencitst ####
+##########################################
+
+### Chapter 4 ###
+
+## List Of Numbers ##
+
+numbers = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for number in numbers:
+    print(number)
+    
+for number in numbers:
+    print("number: %d -- it's square: %d" %(number, number ** 2))
---
+++ Chapter4/MonthsOfTheYear.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 19:48:53 2018
+
+@author: kate
+"""
+
+##########################################
+#### Think Like A Computer Sciencitst ####
+##########################################
+
+### Chapter 4 ###
+
+## Months Of The Year ##
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 
+          'October', 'November', 'December']
+
+for month in months:
+    print("One of the months of the year is " + month + ".")
